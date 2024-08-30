fbaff44cf98ff602b64bc31c4824dcbe459c8972
PorterZer0 <113658429+PorterZer0@users.noreply.github.com>
2024-08-30 20:16:31 +1200

    Create landing.js

---
+++ landing.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.selectable-img');
+
+    images.forEach(image => {
+        image.addEventListener('click', () => {
+            const selectedImageSrc = image.getAttribute('src');
+            localStorage.setItem('selectedImage', selectedImageSrc);
+            window.location.href = 'index.html';
+        });
+    });
+});
